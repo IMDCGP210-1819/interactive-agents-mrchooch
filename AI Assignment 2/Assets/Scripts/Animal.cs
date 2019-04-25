230f00fbb3b06e0c6e4d5fe5daeaae0da401018e
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-04-25 14:48:20 +0100

    Added "Wander" state

--- AI Assignment 2/Assets/Scripts/Animal.cs
+++ AI Assignment 2/Assets/Scripts/Animal.cs
@@ -10,15 +10,42 @@ public class Animal : MonoBehaviour
 	public int thirst;
 	public int tiredness;
 	public animalState state = animalState.Wander;
+	private Vector3 targetPos;
+	private float speed = 0.5f;
+	private float wanderRange = 2;
 
 	private void Start() {
+		targetPos = transform.position;
 		UpdateState();
+	}
+
+	private void Update() {
+		targetPos = new Vector3(Mathf.Clamp(targetPos.x, -8, 8), Mathf.Clamp(targetPos.y, -5, 5), 0);
+		transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+		if (transform.position == targetPos && state == animalState.Wander) {
+			UpdateState();
+		}
 	}
 
 	public void UpdateState() {
 		//Wander
 		if (state == animalState.Wander) {
-			print("X");
+			hunger += Random.Range(1,10);
+			thirst += Random.Range(1, 10);
+			tiredness += Random.Range(1, 10);
+
+			if (Random.value*100 < thirst) {
+				state = animalState.FindWater;
+				UpdateState();
+			} else {
+				targetPos = new Vector3(Random.Range(transform.position.x - wanderRange, transform.position.x + wanderRange), 
+										Random.Range(transform.position.y - wanderRange, transform.position.y + wanderRange), 0);
+			}
+		}
+
+		//FindWater
+		if (state == animalState.FindWater) {
+			print("C");
 		}
 	}
 }
