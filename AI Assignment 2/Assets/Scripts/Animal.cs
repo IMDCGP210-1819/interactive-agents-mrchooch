fc9904bc2f5a42207366e2a25d7a07a9612c89ee
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-04-25 15:17:54 +0100

    Added "FindWater" and "Drink" states

--- AI Assignment 2/Assets/Scripts/Animal.cs
+++ AI Assignment 2/Assets/Scripts/Animal.cs
@@ -16,27 +16,36 @@ public class Animal : MonoBehaviour
 
 	private void Start() {
 		targetPos = transform.position;
-		UpdateState();
+		StartCoroutine(UpdateState());
 	}
 
 	private void Update() {
 		targetPos = new Vector3(Mathf.Clamp(targetPos.x, -8, 8), Mathf.Clamp(targetPos.y, -5, 5), 0);
 		transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
 		if (transform.position == targetPos && state == animalState.Wander) {
-			UpdateState();
+			StartCoroutine(UpdateState());
 		}
 	}
 
-	public void UpdateState() {
+	void OnTriggerStay(Collider collider) {
+		if (collider.gameObject.tag == "Water" && state == animalState.FindWater) {
+			state = animalState.Drink;
+			targetPos = transform.position;
+			StartCoroutine(UpdateState());
+		}
+	}
+
+	public IEnumerator UpdateState() {
 		//Wander
 		if (state == animalState.Wander) {
 			hunger += Random.Range(1,10);
 			thirst += Random.Range(1, 10);
 			tiredness += Random.Range(1, 10);
 
-			if (Random.value*100 < thirst) {
+			if (Random.value*100 < thirst/2) {
 				state = animalState.FindWater;
-				UpdateState();
+				StartCoroutine(UpdateState());
 			} else {
 				targetPos = new Vector3(Random.Range(transform.position.x - wanderRange, transform.position.x + wanderRange), 
 										Random.Range(transform.position.y - wanderRange, transform.position.y + wanderRange), 0);
@@ -45,7 +54,21 @@ public class Animal : MonoBehaviour
 
 		//FindWater
 		if (state == animalState.FindWater) {
-			print("C");
+			float dist1 = Vector3.Distance(GameObject.FindGameObjectsWithTag("Water")[0].transform.position, transform.position);
+			float dist2 = Vector3.Distance(GameObject.FindGameObjectsWithTag("Water")[1].transform.position, transform.position);
+			if (dist1 < dist2) {
+				targetPos = GameObject.FindGameObjectsWithTag("Water")[0].transform.position;
+			} else {
+				targetPos = GameObject.FindGameObjectsWithTag("Water")[1].transform.position;
+			}
+		}
+
+		//Drink
+		if (state == animalState.Drink) {
+			yield return new WaitForSeconds(3);
+			thirst = 0;
+			state = animalState.Wander;
+			StartCoroutine(UpdateState());
 		}
 	}
 }
