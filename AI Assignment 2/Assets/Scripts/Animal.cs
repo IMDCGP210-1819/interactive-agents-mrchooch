07400704fdab7ad8c69bf3c4f37336f1cd5600c1
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-04-25 14:11:32 +0100

    Project Setup

---
+++ AI Assignment 2/Assets/Scripts/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+	public enum animalState {Wander, FindFood, FindWater, Eat, Drink, Sleep, FindMate, Mate};
+
+	public int hunger;
+	public int thirst;
+	public int tiredness;
+	public animalState state = animalState.Wander;
+
+	private void Start() {
+		UpdateState();
+	}
+
+	public void UpdateState() {
+		//Wander
+		if (state == animalState.Wander) {
+			print("X");
+		}
+	}
+}
